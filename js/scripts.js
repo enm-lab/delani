6691cb9c4cffd393b78cf9c9fc1d4ed2ab6bea9c
enm-lab <mwanzaemmaculate@gmail.com>
2019-11-05 12:33:24 +0300

    added hover functions

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $("#design").click(function() {
+      $("#design-icon").toggle('slow');
+      $("#design-p").toggle('slow');
+    });
+    $("#dev").click(function() {
+        $("#dev-icon").toggle('slow');
+        $("#dev-p").toggle('slow');
+    });
+    $("#product").click(function() {
+      $("#product-icon").toggle('slow');
+      $("#product-p").toggle('slow');
+    });
+    $("#work4").hover(function(){
+        $(".work4").toggle('slow');
+    });
+    $("#work3").hover(function(){
+      $(".work3").toggle('slow');
+    });
+    $("#work2").hover(function(){
+      $(".work2").toggle('slow');
+  });
+    $("#work1").hover(function(){
+      $(".work1").toggle('slow');
+    });
+    $("#work5").hover(function(){
+      $(".work5").toggle('slow');
+    });
+    $("#work6").hover(function(){
+      $(".work6").toggle('slow');
+    });
+    $("#work7").hover(function(){
+      $(".work7").toggle('slow');
+    });
+    $("#work8").hover(function(){
+      $(".work8").toggle('slow');
+    });
